617de3fb26bd8bb960efb12c1992cdbe994f73d3
Isaac Lo <isaaclo123@gmail.com>
2018-07-28 19:20:49 -0700

    imgur

--- transcriber_bot/config.py
+++ transcriber_bot/config.py
@@ -21,3 +21,28 @@ USERNAME = 'isaac_lo' # os.environ.get('USERNAME')
 SUBREDDIT_LIST = [
     'testingground4bots',
 ]
+
+"""
+class Config(object):
+    # Config class for the reddit transcriber bot
+
+    # reddit client id
+    CLIENT_ID = 'bx2UN5dyZWVQuw' # os.environ.get('CLIENT_ID')
+
+    # reddit client secret
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+
+    # reddit client password
+    PASSWORD = os.environ.get('PASSWORD')
+
+    # reddit client user agent text
+    USER_AGENT = 'transcriber-bot'
+
+    # reddit client username
+    USERNAME = 'isaac_lo' # os.environ.get('USERNAME')
+
+    # list of subreddits to watch
+    SUBREDDIT_LIST = [
+        'testingground4bots',
+    ]
+"""
--- transcriber_bot/imgur.py
+++ transcriber_bot/imgur.py
@@ -1,27 +1,73 @@
 """Helper methods for managing imgur links"""
 
-from bs4 import BeautifulSoup
+import json
 import requests
+
+def get_imgur_url(imgur_image):
+    """returns imgur image link from imgur image object
+
+    :imgur_image: imgur image object
+        {
+            'hash':'xZEPnZu',
+            'title':'Short bio example',
+            'description':None,
+            'has_sound':False,
+            'width':791,
+            'height':1024,
+            'size':206030,
+            'ext':'.jpg',
+            'animated':False,
+            'prefer_video':False,
+            'looping':False,
+            'datetime':'2015-02-12 13:58:18'
+        }
+    :returns: valid link to imgur image, or None, if invalid
+
+    """
+    imgur_hash = imgur_image.get('hash')
+    imgur_ext = imgur_image.get('ext')
+
+    # checks if the imgur image object has a 'hash' and 'ext' within it
+    if imgur_hash and imgur_ext:
+        return 'https://imgur.com/{hash}{ext}'.format(hash=imgur_hash,
+                                                       ext=imgur_ext)
+
+    # otherwise return None if canot create imgur object
+    return None
+
 
 def get_imgur_urls(imgur_url):
     """Searches for imgur images
 
     :imgur_url: imgur url
     :returns: list of imgur image urls
+
     """
 
-    imgur_page = requests.get(imgur_url)
-    soup = BeautifulSoup(imgur_page, 'html.parser')
-    images = soup.find_all('.post-image-placeholder img')
+    # convert i.imgur.com/xZEPnZu to i.imgur.com/xZEPnZu.json
+    imgur_json = requests.get('{url}.json'.format(url=imgur_url)).text
+    imgur_data = json.loads(imgur_json)
+
+    print(imgur_data)
+
+    images = None
+
+    try:
+        images = imgur_data['data']['image']['album_images']['images']
+    except KeyError:
+        # if images is invalid, print out statement and return empty list
+        print('Invalid imgur data')
+        return []
+
     image_urls = []
 
     for image in images:
-        # add image src attribute contents to image_urls
-        image_src = image.get('src')
-        # because src attributes are in form "//i.imgur.com/xZEPnZu.jpg", add
-        # "https:" to front
-        src_url = "https:{src}".format(src=image_src)
-
-        image_urls.append(src_url)
+        image_link = get_imgur_url(image)
+        # if an image link can be created
+        if image_link:
+            # add it to image_urls
+            image_urls.append(image_link)
 
     return image_urls
+
+print(get_imgur_urls("https://imgur.com/gallery/2OMsd"))
