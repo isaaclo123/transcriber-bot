f1caea9a00bc73e721832672921dca2ef4a789b9
Isaac Lo <isaaclo123@gmail.com>
2018-07-28 19:58:35 -0700

    try catch blocks

--- transcriber_bot/imgur.py
+++ transcriber_bot/imgur.py
@@ -3,38 +3,62 @@
 import json
 import requests
 
+MAX_TIMEOUT = 10 # 10 second timeout
+MAX_IMAGE_COUNT = 10 # only 10 images allowed to be parsed by bot
+
+def get_imgur_data(imgur_url):
+    """get imgur json data from imgur url
+
+    :imgur_url: imgur url
+    :returns: imgur data in python form
+
+    """
+    try:
+        # convert i.imgur.com/xZEPnZu to i.imgur.com/xZEPnZu.json
+        imgur_json = requests.get("{url}.json".format(url=imgur_url),
+                                  timeout=MAX_TIMEOUT).text
+        imgur_data = json.loads(imgur_json)
+        return imgur_data
+    except requests.Timeout:
+        print("Request timed out")
+    except requests.RequestException:
+        print("Internet issue, requests error")
+    except json.decoder.JSONDecodeError:
+        # if images is invalid, print out statement and return empty list
+        print("Invalid JSON data")
+    return None
+
 def get_imgur_url(imgur_image):
     """returns imgur image link from imgur image object
 
     :imgur_image: imgur image object
         {
-            'hash':'xZEPnZu',
-            'title':'Short bio example',
-            'description':None,
-            'has_sound':False,
-            'width':791,
-            'height':1024,
-            'size':206030,
-            'ext':'.jpg',
-            'animated':False,
-            'prefer_video':False,
-            'looping':False,
-            'datetime':'2015-02-12 13:58:18'
+            "hash":"xZEPnZu",
+            "title":"Short bio example",
+            "description":None,
+            "has_sound":False,
+            "width":791,
+            "height":1024,
+            "size":206030,
+            "ext":".jpg",
+            "animated":False,
+            "prefer_video":False,
+            "looping":False,
+            "datetime":"2015-02-12 13:58:18"
         }
     :returns: valid link to imgur image, or None, if invalid
 
     """
-    imgur_hash = imgur_image.get('hash')
-    imgur_ext = imgur_image.get('ext')
+    imgur_hash = imgur_image.get("hash")
+    imgur_ext = imgur_image.get("ext")
 
-    # checks if the imgur image object has a 'hash' and 'ext' within it
+    # checks if the imgur image object has a "hash" and "ext" within it
     if imgur_hash and imgur_ext:
-        return 'https://imgur.com/{hash}{ext}'.format(hash=imgur_hash,
-                                                       ext=imgur_ext)
+        return "https://imgur.com/{hash}{ext}".format(hash=imgur_hash,
+                                                      ext=imgur_ext)
 
     # otherwise return None if canot create imgur object
     return None
-
 
 def get_imgur_urls(imgur_url):
     """Searches for imgur images
@@ -44,30 +68,38 @@ def get_imgur_urls(imgur_url):
 
     """
 
-    # convert i.imgur.com/xZEPnZu to i.imgur.com/xZEPnZu.json
-    imgur_json = requests.get('{url}.json'.format(url=imgur_url)).text
-    imgur_data = json.loads(imgur_json)
+    # get imgur json data
+    imgur_data = get_imgur_data(imgur_url)
 
-    print(imgur_data)
-
-    images = None
-
-    try:
-        images = imgur_data['data']['image']['album_images']['images']
-    except KeyError:
-        # if images is invalid, print out statement and return empty list
-        print('Invalid imgur data')
+    if not imgur_data:
+        # if imgur data invalid, return empty list
         return []
 
-    image_urls = []
+    try:
+        images = imgur_data["data"]["image"]["album_images"]["images"]
+        image_urls = []
 
-    for image in images:
-        image_link = get_imgur_url(image)
-        # if an image link can be created
-        if image_link:
-            # add it to image_urls
-            image_urls.append(image_link)
+        image_count = 0
+        for image in images:
+            image_link = get_imgur_url(image)
+            # if an image link can be created
+            if image_link:
+                # add it to image_urls
+                image_urls.append(image_link)
+                image_count += 1
 
-    return image_urls
+                if image_count >= MAX_IMAGE_COUNT:
+                    # break if album has too many images
+                    break
 
-print(get_imgur_urls("https://imgur.com/gallery/2OMsd"))
+        return image_urls
+
+    except KeyError:
+        # if images is invalid, print out statement and return empty list
+        print("Invalid imgur data, KeyError")
+    except ValueError:
+        # if images is invalid, print out statement and return empty list
+        print("Invalid imgur data, ValueError")
+    return []
+
+print(get_imgur_urls("https://imgur.com/gallery/nHyGiG3"))
